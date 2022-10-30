39b101b2c6949f21962af5f5bbe49f3cbf58b914
bengal865 <69059209+bengal865@users.noreply.github.com>
2022-10-30 07:32:47 -0400

    Added new file.

---
+++ function_with_while_loop.py
@@ -0,0 +1,15 @@
+# Add your name here
+# Current date
+# Function with While Loop
+
+# Instructions
+
+# Read pages 141 - 142 in Python Crash Course
+
+# In this Python file, enter the code shown on
+# pages 141 - 142
+
+# Make sure you define a quit condition so your while 
+# loop doesn't become an infinite loop!
+
+# Test your script and debug it if necessary
